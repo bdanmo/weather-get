0e6088164df2be7b2d6c75381a75fa60ca220ca9
Brendan Moran <b.daniel.moran@gmail.com>
2020-08-22 19:35:52 -0400

    added jsdoc for util functions

--- modules/util.js
+++ modules/util.js
@@ -1,7 +1,17 @@
+/**
+ * Receives degrees kelvin and returns degrees fahrenheit
+ * @param {number} K - degrees kelvin
+ * @returns {number} degrees fahrenheit
+ */
 function convertTemp(K) {
   return Math.round(((K - 273.15) * 9) / 5 + 32);
 }
 
+/**
+ * Receives a unix time stamp and returns a simple hours:minutes time (24 hr)
+ * @param {number} unix - a unix time stamp
+ * @returns  {string} 24 hour time in hours:minutes
+ */
 function time(unix) {
   const date = new Date(unix * 1000);
   const hour = date.getHours();
